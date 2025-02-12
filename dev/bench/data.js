329a1066055e799e55faaa11cf951c4474c2e640
github-action-benchmark <github@users.noreply.github.com>
2025-02-12 16:14:50 +0000

    add Rust Benchmark (cargo) benchmark result for dc95b590c78ff6e7f2aa9956378dbac9a3a80fc0

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1739376890232,
+  "repoUrl": "https://github.com/crazyjackel/asset-importer-rs",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jackel1020@gmail.com",
+            "name": "J L",
+            "username": "crazyjackel"
+          },
+          "committer": {
+            "email": "jackel1020@gmail.com",
+            "name": "J L",
+            "username": "crazyjackel"
+          },
+          "distinct": true,
+          "id": "dc95b590c78ff6e7f2aa9956378dbac9a3a80fc0",
+          "message": "Hotfix #2\n\nBenchmarking did not work due criterion and lib bench... ideally this fixes it when run locally",
+          "timestamp": "2025-02-12T11:12:54-05:00",
+          "tree_id": "92627ca61d616ccc7af770ce55be475ba6140977",
+          "url": "https://github.com/crazyjackel/asset-importer-rs/commit/dc95b590c78ff6e7f2aa9956378dbac9a3a80fc0"
+        },
+        "date": 1739376889339,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Import Group/import avocado",
+            "value": 96121952,
+            "range": "± 303443",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Export Group/export avocado",
+            "value": 196740673,
+            "range": "± 431941",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
