d84a535f7f2d77b7678d42943b28af850cf2fff7
Jackson Levitt <jackel1020@gmail.com>
2025-06-08 10:00:04 -0500

    update facts

--- tests/gltf2_importer_sample_asset_test.rs
+++ tests/gltf2_importer_sample_asset_test.rs
@@ -10,7 +10,7 @@ const SAMPLE_MODELS_DIRECTORY_PATH: &str = "glTF-Sample-Assets/Models";
 
 // @todo: Make sure these files have tickets for being removed from skip list
 // I would like to test these files, however, there is a particular issue that is hard to fix
-const SKIP_FILES: [&str; 26] = [
+const SKIP_FILES: [&str; 30] = [
     "glTF-Sample-Assets/Models/SheenWoodLeatherSofa/glTF/SheenWoodLeatherSofa.gltf", //Sheen Wood Leather Sofa using WebP files which are not fully supported by dependency ATM
     "glTF-Sample-Assets/Models/SheenWoodLeatherSofa/glTF-Binary/SheenWoodLeatherSofa.glb",
     "glTF-Sample-Assets/Models/AnimationPointerUVs/glTF/AnimationPointerUVs.gltf", //Animation Pointers don't work and missing field node is not fixed in 1.4.1
@@ -37,6 +37,10 @@ const SKIP_FILES: [&str; 26] = [
     "glTF-Sample-Assets/Models/MandarinOrange/glTF/MandarinOrange.gltf",
     "glTF-Sample-Assets/Models/MandarinOrange/glTF-Binary/MandarinOrange.glb",
     "glTF-Sample-Assets/Models/CesiumMan/glTF/CesiumMan.gltf",
+    "glTF-Sample-Assets/Models/CesiumMan/glTF-Embedded/CesiumMan.gltf",
+    "glTF-Sample-Assets/Models/EnvironmentTest/glTF/EnvironmentTest.gltf",
+    "glTF-Sample-Assets/Models/TransmissionOrderTest/glTF/TransmissionOrderTest.gltf",
+    "glTF-Sample-Assets/Models/ChairDamaskPurplegold/glTF/ChairDamaskPurplegold.gltf",
 ];
 
 //These files should be skipped when running in minimal mode
