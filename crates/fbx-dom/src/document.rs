e4acca055bcc57ff29a454e47109c3d21d460225
Jackson Levitt <jackel1020@gmail.com>
2025-11-16 14:19:56 -0500

    formatting fixes

--- crates/fbx-dom/src/document.rs
+++ crates/fbx-dom/src/document.rs
@@ -1,5 +1,5 @@
-use fbxscii::{ElementAmphitheatre, ElementParseError, Parser, ParserError};
 use fbxcel::tree::v7400::Tree;
+use fbxscii::{ElementAmphitheatre, ElementParseError, Parser, ParserError};
 use std::io::BufRead;
 
 const LOWEST_SUPPORTED_VERSION: u32 = 7100;
@@ -15,22 +15,29 @@ pub enum DocumentParseError {
 
 #[derive(Debug, Default)]
 pub struct ImportSettings {
-    pub strict: bool
+    pub strict: bool,
 }
 
 #[derive(Default)]
 pub struct Document {
     fbx_version: u32,
     creator: String,
-    creation_date: [u32; 7]
+    creation_date: [u32; 7],
 }
 
 trait HeaderReader {
-    fn read_header(&self, document: &mut Document, settings: ImportSettings) -> Result<(), DocumentParseError>;
+    fn read_header(
+        &self,
+        document: &mut Document,
+        settings: ImportSettings,
+    ) -> Result<(), DocumentParseError>;
 }
 
 impl Document {
-    pub fn from_parser<R>(parser: Parser<R>, settings: ImportSettings) -> Result<Self, DocumentParseError>
+    pub fn from_parser<R>(
+        parser: Parser<R>,
+        settings: ImportSettings,
+    ) -> Result<Self, DocumentParseError>
     where
         R: BufRead,
     {
@@ -42,39 +49,105 @@ impl Document {
 }
 
 impl HeaderReader for ElementAmphitheatre {
-
-    fn read_header(&self, document: &mut Document, settings: ImportSettings) -> Result<(), DocumentParseError> {
+    fn read_header(
+        &self,
+        document: &mut Document,
+        settings: ImportSettings,
+    ) -> Result<(), DocumentParseError> {
         let header_extension = self.get_handle_by_key("FBXHeaderExtension");
         if header_extension.is_none() {
-            return Err(DocumentParseError::RequiredElementNotFound("FBXHeaderExtension".to_string()));
+            return Err(DocumentParseError::RequiredElementNotFound(
+                "FBXHeaderExtension".to_string(),
+            ));
         }
         let header_extension = header_extension.unwrap();
-        let version_element = header_extension.first_child_by_key("FBXVersion").ok_or(DocumentParseError::RequiredElementNotFound("FBXVersion".to_string()))?;
-        document.fbx_version = version_element.try_into().map_err(DocumentParseError::ElementParseError)?;
+
+        // Handle FBXVersion element
+        let version_element = header_extension.first_child_by_key("FBXVersion").ok_or(
+            DocumentParseError::RequiredElementNotFound("FBXVersion".to_string()),
+        )?;
+        document.fbx_version = version_element
+            .try_into()
+            .map_err(DocumentParseError::ElementParseError)?;
+
+        // Check if the version is supported
         if document.fbx_version < LOWEST_SUPPORTED_VERSION {
-            return Err(DocumentParseError::UnsupportedVersion(document.fbx_version, None));
+            return Err(DocumentParseError::UnsupportedVersion(
+                document.fbx_version,
+                None,
+            ));
         }
         if document.fbx_version > UPPER_SUPPORTED_VERSION && settings.strict {
-            return Err(DocumentParseError::UnsupportedVersion(document.fbx_version, Some("Turn off strict mode to import this version.".to_string())));
+            return Err(DocumentParseError::UnsupportedVersion(
+                document.fbx_version,
+                Some("Turn off strict mode to import this version.".to_string()),
+            ));
         }
-        let creator_element = header_extension.first_child_by_key("Creator").ok_or(DocumentParseError::RequiredElementNotFound("Creator".to_string()))?;
-        document.creator = creator_element.try_into().map_err(DocumentParseError::ElementParseError)?;
 
-        let creation_date_element = header_extension.first_child_by_key("CreationTimeStamp").ok_or(DocumentParseError::RequiredElementNotFound("CreationTimeStamp".to_string()))?;
-        let year_element = creation_date_element.first_child_by_key("Year").ok_or(DocumentParseError::RequiredElementNotFound("Year".to_string()))?;
-        let year = year_element.try_into().map_err(DocumentParseError::ElementParseError)?;
-        let month_element = creation_date_element.first_child_by_key("Month").ok_or(DocumentParseError::RequiredElementNotFound("Month".to_string()))?;
-        let month = month_element.try_into().map_err(DocumentParseError::ElementParseError)?;
-        let day_element = creation_date_element.first_child_by_key("Day").ok_or(DocumentParseError::RequiredElementNotFound("Day".to_string()))?;
-        let day = day_element.try_into().map_err(DocumentParseError::ElementParseError)?;
-        let hour_element = creation_date_element.first_child_by_key("Hour").ok_or(DocumentParseError::RequiredElementNotFound("Hour".to_string()))?;
-        let hour = hour_element.try_into().map_err(DocumentParseError::ElementParseError)?;
-        let minute_element = creation_date_element.first_child_by_key("Minute").ok_or(DocumentParseError::RequiredElementNotFound("Minute".to_string()))?;
-        let minute = minute_element.try_into().map_err(DocumentParseError::ElementParseError)?;
-        let second_element = creation_date_element.first_child_by_key("Second").ok_or(DocumentParseError::RequiredElementNotFound("Second".to_string()))?;
-        let second = second_element.try_into().map_err(DocumentParseError::ElementParseError)?;
-        let millisecond_element = creation_date_element.first_child_by_key("Millisecond").ok_or(DocumentParseError::RequiredElementNotFound("Millisecond".to_string()))?;
-        let millisecond = millisecond_element.try_into().map_err(DocumentParseError::ElementParseError)?;
+        // Handle Creator element
+        document.creator = header_extension
+            .first_child_by_key("Creator")
+            .ok_or(DocumentParseError::RequiredElementNotFound(
+                "Creator".to_string(),
+            ))?
+            .try_into()
+            .map_err(DocumentParseError::ElementParseError)?;
+
+        // Handle CreationTimeStamp element    
+        let creation_date_element = header_extension
+            .first_child_by_key("CreationTimeStamp")
+            .ok_or(DocumentParseError::RequiredElementNotFound(
+                "CreationTimeStamp".to_string(),
+            ))?;
+        let year = creation_date_element
+            .first_child_by_key("Year")
+            .ok_or(DocumentParseError::RequiredElementNotFound(
+                "Year".to_string(),
+            ))?
+            .try_into()
+            .map_err(DocumentParseError::ElementParseError)?;
+        let month = creation_date_element
+            .first_child_by_key("Month")
+            .ok_or(DocumentParseError::RequiredElementNotFound(
+                "Month".to_string(),
+            ))?
+            .try_into()
+            .map_err(DocumentParseError::ElementParseError)?;
+        let day = creation_date_element
+            .first_child_by_key("Day")
+            .ok_or(DocumentParseError::RequiredElementNotFound(
+                "Day".to_string(),
+            ))?
+            .try_into()
+            .map_err(DocumentParseError::ElementParseError)?;
+        let hour = creation_date_element
+            .first_child_by_key("Hour")
+            .ok_or(DocumentParseError::RequiredElementNotFound(
+                "Hour".to_string(),
+            ))?
+            .try_into()
+            .map_err(DocumentParseError::ElementParseError)?;
+        let minute = creation_date_element
+            .first_child_by_key("Minute")
+            .ok_or(DocumentParseError::RequiredElementNotFound(
+                "Minute".to_string(),
+            ))?
+            .try_into()
+            .map_err(DocumentParseError::ElementParseError)?;
+        let second = creation_date_element
+            .first_child_by_key("Second")
+            .ok_or(DocumentParseError::RequiredElementNotFound(
+                "Second".to_string(),
+            ))?
+            .try_into()
+            .map_err(DocumentParseError::ElementParseError)?;
+        let millisecond = creation_date_element
+            .first_child_by_key("Millisecond")
+            .ok_or(DocumentParseError::RequiredElementNotFound(
+                "Millisecond".to_string(),
+            ))?
+            .try_into()
+            .map_err(DocumentParseError::ElementParseError)?;
         document.creation_date = [year, month, day, hour, minute, second, millisecond];
         Ok(())
     }
@@ -87,7 +160,7 @@ mod tests {
     use std::io::BufReader;
 
     use crate::document::{Document, ImportSettings};
-    
+
     #[test]
     fn test_document_parse() {
         let test_document = r#"
