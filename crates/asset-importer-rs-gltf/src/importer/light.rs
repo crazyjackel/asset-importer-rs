549aa0e54e032e3460551538d66b2c08f886172a
martinfrances107 <martinfrances107@hotmail.com>
2026-08-11 21:27:57 +0100

    Perf: Removed allocation caused by a call to .collect().

--- crates/asset-importer-rs-gltf/src/importer/light.rs
+++ crates/asset-importer-rs-gltf/src/importer/light.rs
@@ -17,8 +17,8 @@ impl Gltf2Importer {
             return Ok(Vec::new());
         }
         let mut lights: Vec<AiLight> = Vec::new(); //Final Lights to return
-        let asset_lights: Vec<gltf::khr_lights_punctual::Light<'_>> =
-            document.lights().unwrap().collect();
+        let asset_lights =
+            document.lights().unwrap();
         lights.reserve(asset_lights.len());
         for light in asset_lights {
             let mut ai_light = AiLight::default();
