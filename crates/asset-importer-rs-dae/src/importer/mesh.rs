539082a036c172b4ad516d5515431b47a4a60087
Jackson Levitt <jackel1020@gmail.com>
2026-08-16 20:50:22 -0400

    fix: cargo fmt

--- crates/asset-importer-rs-dae/src/importer/mesh.rs
+++ crates/asset-importer-rs-dae/src/importer/mesh.rs
@@ -697,8 +697,8 @@ mod tests {
         assert_eq!(
             mesh.face_pos_indices,
             vec![
-                0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16,
-                17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
+                0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15,
+                16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
             ]
         );
     }
@@ -758,9 +758,8 @@ mod tests {
         let library = importer
             .import_mesh_library(&document)
             .expect("mesh library");
-        let (materials, material_index_map) = importer
-            .import_materials(&document)
-            .expect("materials");
+        let (materials, material_index_map) =
+            importer.import_materials(&document).expect("materials");
         assert_eq!(materials.len(), 1);
 
         let node = document
@@ -786,4 +785,3 @@ mod tests {
         assert_eq!(mesh.vertices[0], AiVector3D::new(-50.0, 50.0, 50.0));
     }
 }
-
